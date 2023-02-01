14a897c941c5edad743a30f547877e74964b13b0
Nishit Bheda <nbheda@uwaterloo.ca>
2023-01-31 22:30:33 -0500

    Fixed date picker value resetting bug

---
+++ setDate.js
@@ -0,0 +1,8 @@
+// function setDate() {
+//     const date = document.getElementById("date");
+//     const yesterday = new Date();
+//     yesterday.setDate(yesterday.getDate()-1);
+//     date.valueAsDate = yesterday;
+//     date.setAttribute("value", date );
+//     console.log("ABC");
+// }
